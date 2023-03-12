208e8ce04a038099ef87fa7a6abc9d08276442d2
rafaelvieira10 <rafael.rvs23@gmail.com>
2023-03-12 16:54:34 -0300

    pagina de inicio

---
+++ Desktop/portfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+import Inicio from './components/Inicio';
+
+
+function App() {
+  return (
+    <div>
+       <Header />
+    <Inicio />
+    </div>
+   
+  );
+}
+
+export default App;
