247b9a495f84498d69cf5b0cfdd4a24fda2e9681
rafaelvieira10 <rafael.rvs23@gmail.com>
2023-03-13 20:56:38 -0300

    carrosel

--- src/components/Projetos/index.js
+++ src/components/Projetos/index.js
@@ -1,36 +1,64 @@
-import './style.css'
-import Slider from "react-slick";
+import "./style.css";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 function Projetos() {
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   initialSlide: 0,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1200,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
 
-    return(
-        <div className="containerProjetos">
-            <div className='divTitle'>
-                <h2>Projetos</h2>
-            </div>
+  return (
+    <div className="containerProjetos">
+      <div className="divTitle">
+        <h2>Projetos</h2>
+      </div>
 
-            <div className='containerCarrosel'>
-            <Slider {...settings}>
-      <div>
-        <h3>1</h3>
+      <div className="containerCarrosel">
+        {/* <Slider {...settings}> */}
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+        {/* </Slider> */}
       </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-    </Slider>
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Projetos;
