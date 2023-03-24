f1bab6cdcef8d5331e339cc13b6cc7aab4ee10b3
rafaelvieira10 <rafael.rvs23@gmail.com>
2023-03-24 11:38:30 -0300

    menu mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,69 @@
 import React from "react";
 import Inicio from "../Inicio";
 import Sobre from "../Sobre";
-import './style.css'
-
+import "./style.css";
+import { useState } from "react";
 
 function Header() {
+  const [abrir, setAbrir] = useState("");
+  const [buttoX, setButtonX] = useState("");
+
+  function abrirMenu() {
+    if (abrir == "") {
+      setAbrir("active");
+      setButtonX("active-button");
+    } else {
+      setAbrir("");
+      setButtonX("");
+    }
+  }
+
+  function fecharMenu() {
+    if (abrir == "") {
+      setAbrir("active");
+    } else {
+      setAbrir("");
+      setButtonX("");
+    }
+  }
+
   return (
     <div className="container" id="header">
       <div className="divLogo">
         <a href="#" className="logo">
-            RV
+          RV
         </a>
       </div>
       <div className="listHeader">
-        <nav className="navList">
-            <ul>
-              <li>
-                <a href="#header">Ínicio</a>
-              </li>
-              <li>
-                <a href="#sobre">Sobre</a>
-              </li>
-              <li>
-                <a href="#projetos">Projetos</a>
-              </li>
-              <li>
-                <a href="#habilidades">Habilidades</a>
-              </li>
-            </ul>
-        </nav>
+        {/* <nav className={`navList ${abrir}`} id="navList" > */}
+        <ul className={`navList ${abrir}`} id="navList">
+          <li>
+            <a href="#header" onClick={fecharMenu}>
+              Ínicio
+            </a>
+          </li>
+          <li>
+            <a href="#sobre" onClick={fecharMenu}>
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a href="#projetos" onClick={fecharMenu}>
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a href="#habilidades" onClick={fecharMenu}>
+              Habilidades
+            </a>
+          </li>
+        </ul>
+        <div className={`menu-mobile ${buttoX}`} onClick={abrirMenu}>
+          <div className="linha1"></div>
+          <div className="linha2"></div>
+          <div className="linha3"></div>
+        </div>
+        {/* </nav> */}
       </div>
     </div>
   );
